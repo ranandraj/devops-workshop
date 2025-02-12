522fa3ce66c0b237fa8921f1f071cf6f4d9c31db
ranandraj <ranandraj@gmail.com>
2025-02-12 15:05:03 +0530

    Corrected powershell path

--- Run-powershell-Enabling-Powershell-Remoting.bat
+++ Run-powershell-Enabling-Powershell-Remoting.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Set the PowerShell execution policy to Bypass temporarily and run the PowerShell script
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "Enabling-Powershell-Remoting.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Enabling-Powershell-Remoting.ps1"
 
 :: Pause for any output (optional)
 pause
